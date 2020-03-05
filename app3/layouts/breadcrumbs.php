09f19fdb8387e2854fc0de5847f2195c1ecdb0d6
Rafles Nainggolan <raflesngln@gmail.com>
2020-03-06 02:12:08 +0700

    edit

---
+++ app3/layouts/breadcrumbs.php
@@ -0,0 +1,9 @@
+<div class="page-header">
+  <h3 class="page-title"> Breadcrumbs </h3>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+    </ol>
+  </nav>
+</div>
---
+++ app3/layouts/sidebar.php
@@ -0,0 +1,68 @@
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item nav-category">Main</li>
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.html">
+                <span class="icon-bg"><i class="mdi mdi-cube menu-icon"></i></span>
+                <span class="menu-title">Dashboard</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#page-layouts" aria-expanded="false" aria-controls="page-layouts">
+                <span class="icon-bg"> <i class="mdi mdi-apps menu-icon"></i> </span>
+                <span class="menu-title">Page Layouts</span>
+                <i class="menu-arrow"></i>
+              </a>
+              <div class="collapse" id="page-layouts">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="index.html">Index</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="../../pages/layout/boxed-layout.html">Boxed</a></li>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#sidebar-layouts" aria-expanded="false" aria-controls="sidebar-layouts">
+                <span class="icon-bg"><i class="mdi mdi-playlist-play menu-icon"></i></span>
+                <span class="menu-title">Sidebar Layouts</span>
+                <i class="menu-arrow"></i>
+              </a>
+              <div class="collapse" id="sidebar-layouts">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="../../pages/layout/compact-menu.html">Compact menu</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="../../pages/layout/sidebar-collapsed.html">Icon menu</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="../../pages/layout/sidebar-hidden.html">Sidebar Hidden</a></li>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="widgets.html">
+                <span class="icon-bg"><i class="mdi mdi-widgets menu-icon"></i></span>
+                <span class="menu-title">Widgets</span>
+              </a>
+            </li>
+          
+            <li class="nav-item nav-category">Applications</li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#e-commerce" aria-expanded="false" aria-controls="e-commerce">
+                <span class="icon-bg"><i class="mdi mdi-shopping menu-icon"></i></span>
+                <span class="menu-title">E-commerce</span>
+                <i class="menu-arrow"></i>
+              </a>
+              <div class="collapse" id="e-commerce">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="invoice.html"> Invoice </a></li>
+                  <li class="nav-item"> <a class="nav-link" href="pricing-table.html"> Pricing Table </a></li>
+                  <li class="nav-item"> <a class="nav-link" href="orders.html"> Orders </a></li>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kanban-board.html">
+                <span class="icon-bg"><i class="mdi mdi-tablet-dashboard menu-icon"></i></span>
+                <span class="menu-title">Kanban Board</span>
+              </a>
+            </li>
+            <li>&nbsp;</li>        
+            
+          </ul>
+        </nav>
